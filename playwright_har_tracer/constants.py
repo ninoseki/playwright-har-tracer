4dafcc220c60acb80521a027fc3d2cd419dc3771
Manabu Niseki <manabu.niseki@gmail.com>
2021-10-11 07:28:03 +0900

    refactor: use importlib.metadata to load the version

--- playwright_har_tracer/constants.py
+++ playwright_har_tracer/constants.py
@@ -1,8 +1,11 @@
-__version__ = "0.3.1"
+import importlib.metadata
 
+NAME: str = "playwright-har-tracer"
+
+__version__ = importlib.metadata.version(NAME)
 
 HAR_VERSION: str = "1.2"
-CREATOR_NAME: str = "playwright-har-tracer"
+CREATOR_NAME: str = NAME
 CREATOR_VERSION: str = __version__
 
 FALLBACK_HTTP_VERSION: str = "HTTP/1.1"
